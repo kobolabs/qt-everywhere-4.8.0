d400894acd8593dbbb3510f26fb772a2598adfd9
Robert Beghian <rbeghian@kobo.com>
2012-02-17 05:48:20 +0000

    Merge for r27539: Fix QURL more
    Former-commit-id: 71e67618f1e1cefe2d9ecc7ab11959dfeb5311c4
    Former-commit-id: 62657bf82c27020d61bdf6c5818376588610b574

--- src/corelib/io/qurl.cpp
+++ src/corelib/io/qurl.cpp
@@ -5436,7 +5436,12 @@ void QUrl::removeEncodedQueryItem(const QByteArray &key)
 
 	    if (pos != 0 && query[pos-1] == d->pairDelimiter)
 	    {
-		    d->query.remove(pos - 1, end - pos + 1);
+		    if (end == d->query.size()) {
+			d->query.remove(pos - 1, end - pos + 1);
+		    } else {
+			d->query.remove(pos - 1, end - pos);
+		    }
+
 	    } else {
 		    d->query.remove(pos, end - pos);
 	    }
