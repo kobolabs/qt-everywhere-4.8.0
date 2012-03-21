98433bace74d3c42e5c060d2b32deb0bd6e37d88
George Talusan <gtalusan@kobo.com>
2012-03-21 03:54:58 +0000

    otf fonts
    Former-commit-id: 3208386f27df7d66a533e3933e0363e9f95e455f
    Former-commit-id: e6b048d01fe943f270673d8803e0b3e2c68f6cca

--- src/gui/text/qfontdatabase_qws.cpp
+++ src/gui/text/qfontdatabase_qws.cpp
@@ -360,7 +360,7 @@ static void initializeDb()
 #ifndef QT_NO_FREETYPE
     dir.setNameFilters(QStringList() << QLatin1String("*.ttf")
                        << QLatin1String("*.ttc") << QLatin1String("*.pfa")
-                       << QLatin1String("*.pfb"));
+                       << QLatin1String("*.pfb") << QLatin1String("*.otf"));
     dir.refresh();
     for (int i = 0; i < int(dir.count()); ++i) {
         const QByteArray file = QFile::encodeName(dir.absoluteFilePath(dir[i]));
