d29f10e802a6df73d0746e41e9bc49dc63e774bd
George Talusan <gtalusan@kobo.com>
2012-04-13 19:36:58 +0000

    add the file please
    Former-commit-id: 6c1438f7c0c59d57b31e60d38c3ed3eb60d724e7
    Former-commit-id: 19d21426feae88c16f0cf95b3dfe610a91f2cf8f

---
+++ src/3rdparty/webkit/Source/WebKit/qt/Api/qwebkitengineversion.h
@@ -0,0 +1,11 @@
+/*
+    Copyright (c) 2012 ACCESS CO., LTD. All rights reserved.
+*/
+
+#ifndef qwebkitengineversion_h
+#define qwebkitengineversion_h
+
+#define WEBKIT_ENGINE_VERSION "20120411"
+
+#endif // qwebkitengineversion_h
+
