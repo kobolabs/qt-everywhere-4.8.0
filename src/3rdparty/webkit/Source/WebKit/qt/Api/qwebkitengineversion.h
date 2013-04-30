317bfcb8fb1d26997f7a6947e84ac1c18f522923
IKEUCHI Yasuki <Yasuki.Ikeuchi@access-company.com>
2013-04-30 12:10:17 -0700

    update version for 20130430 and releasenote

--- src/3rdparty/webkit/Source/WebKit/qt/Api/qwebkitengineversion.h
+++ src/3rdparty/webkit/Source/WebKit/qt/Api/qwebkitengineversion.h
@@ -1,11 +1,11 @@
 /*
-    Copyright (c) 2012 ACCESS CO., LTD. All rights reserved.
+    Copyright (c) 2013 ACCESS CO., LTD. All rights reserved.
 */
 
 #ifndef qwebkitengineversion_h
 #define qwebkitengineversion_h
 
-#define WEBKIT_ENGINE_VERSION "20120731"
+#define WEBKIT_ENGINE_VERSION "20130430"
 
 #endif // qwebkitengineversion_h
 
