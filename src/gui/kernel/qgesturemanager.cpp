3627e3f0920de13a3071cf5c62284d77773296a9
David Lapchuk <dlapchuk@kobo.com>
2013-08-12 15:32:41 -0400

    Oops - QGesture state is read-only
    Revert "Set correct cancelled gesture state when cancelling one finger gestures because of two finger gestures."
    This reverts commit 2304113c3e0b3a0f98c236af376b63b4a73865fe.

--- src/gui/kernel/qgesturemanager.cpp
+++ src/gui/kernel/qgesturemanager.cpp
@@ -264,7 +264,6 @@ bool QGestureManager::filterEventThroughContexts(const QMultiMap<QObject *,
             if (event->type() >= QEvent::TouchBegin && event->type() <= QEvent::TouchEnd &&
                 static_cast<QTouchEvent *>(event)->globalTouchPointCount() > recognizer->maxGlobalTouchPoints()) {
                 recognizerResult = QGestureRecognizer::CancelGesture;
-                state->setProperty("state", Qt::GestureCanceled);
             }
             else {
                 recognizerResult = recognizer->recognize(state, target, event);
